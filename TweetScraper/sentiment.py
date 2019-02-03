f2ea60c13f78650f1ce79132295e81e72c0fb559
Noah Rottman <noahrottman@Noahs-MacBook-Pro.local>
2019-02-02 19:34:58 -0500

    fixed up sentiment

--- TweetScraper/sentiment.py
+++ TweetScraper/sentiment.py
@@ -16,6 +16,7 @@ data = pd.read_csv("data.csv", names=colnames)
 text = data.text.tolist()
 date = data.datetime.tolist()
 text_and_date = np.column_stack((text, date))
+score = []
 avg = 0
 cnt = 0
 pos = 0
@@ -23,6 +24,7 @@ neg = 0
 neutral = 0
 for i in range(len(text)):
     temp = TextBlob(text[i]).sentiment.polarity
+    score.append(temp)
     avg += temp
     cnt += 1
     if temp > 0:
@@ -32,6 +34,7 @@ for i in range(len(text)):
     else:
         neg += 1
     #print(text[i], ": ", temp)
+text_and_date = np.column_stack((date, text, score))
 avge = avg/cnt
 print("Average: ", avge)
 print("Positive: ", pos)
